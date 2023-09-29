82c60739f6d84c4dafc867452b2fe4067fee15da
GoldenDovah <bilal-mok@hotmail.fr>
2023-09-30 00:37:17 +0100

    added static showcase of requests

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,9 +17,59 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Intrusion Detection System'),
+        title: const Text('Intrusion Detection System'),
       ),
-      body: const Placeholder(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.0),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: const Column(
+                children: [
+                  Text(
+                    'Number of Requests',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    '42', // Replace with your actual number
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 32.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                buildRequestTypeContainer('Benign', Icons.security, '10'),
+                buildRequestTypeContainer('DDOS', Icons.warning, '15'),
+                buildRequestTypeContainer('Port Scan', Icons.error, '17'),
+              ],
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
@@ -37,6 +87,54 @@ class _HomePageState extends State<HomePage> {
             label: 'Requests',
           ),
         ],
+      ),
+    );
+  }
+
+  Widget buildRequestTypeContainer(String text, IconData icon, String number) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.0),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                text,
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16.0,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Icon(
+                icon,
+                size: 32.0,
+                color: text != 'Benign' ? Colors.red : Colors.green,
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                number, // Replace with your actual number
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
